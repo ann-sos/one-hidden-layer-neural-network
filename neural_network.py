76e60d7ad0bfd7423f82c53af718bd59e446f8f2
Adrian <44926230+daddyaddy@users.noreply.github.com>
2021-12-29 22:42:06 +0100

    Proposal backward_propagation

--- neural_network.py
+++ neural_network.py
@@ -12,6 +12,9 @@ def sigmoid(x, Beta=1):
     "Activation function"
     return 1/(1 + np.exp(- Beta * x))
 
+def sigmoid_prime(self, z):
+    return sigmoid(z)*(1-sigmoid(z))
+
 def cost_function(Y_predicted, Y_expected):
     """MSE= Mean Squared error"""
     return 1 / Y_expected.shape[0] * np.sum((Y_predicted - Y_expected) ** 2)
@@ -61,19 +64,35 @@ def forward_propagation(X, hidden_neurons, output_neurons, parameters):
         B_layer[f"B{i+1}"] = sigmoid(sumator(X, ifarray2(parameters['B_weights'], i), ifarray(parameters['B_bias'], i))) 
     return B_layer
 
+def backward_propagation(
+    X: pd.DataFrame, 
+    Y: pd.DataFrame, 
+    neurons: int,
+    errors: list,
+    inputs: list,
+    outputs: pd.DataFrame, 
+    weights: list
+):
+    errors[neurons-1] = outputs[neurons-1] - Y
 
-def backward_propagation(X, Y, Y_predicted, cost, parameters):
-    #chain rule
-
-    #gradient descent
-    return parameters
+    for i in range(neurons-2, 0, -1):
+        errors[i] = np.dot(errors[i+1], weights[i][:,1:]) * sigmoid_prime(inputs[i])
+    
+    for i in range(0, neurons-1):
+        grad = np.dot(np.transpose(errors[i+1]), outputs[i]) / X.shape[0]
+        weights[i] = weights[i] - 0.2 * grad
+    
+    return weights
 
 def train_neural_network(X: pd.DataFrame, Y: pd.DataFrame, epochs: int, hidden_neurons: int, output_neurons: int):
     parameters = generate_parameters(X.shape[1], hidden_neurons, output_neurons)
+    # errors = []
+    # for layer in range(neurons):
+        # errors.append(np.empty([X.shape[0], neurons[layer]]))
     for i in range(epochs):
         Y_predicted = forward_propagation(X, hidden_neurons, output_neurons, parameters)
         cost = cost_function(Y_predicted.squeeze(), Y)
-        parameters = backward_propagation(X, Y, Y_predicted, cost, parameters)
+        # parameters = backward_propagation(X, Y, Y_predicted, cost, parameters)
     return parameters
 
 
