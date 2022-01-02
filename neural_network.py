8e57585f2a8cde9be2921f7595cddd198d68d0ca
Adrian <44926230+daddyaddy@users.noreply.github.com>
2022-01-02 17:35:27 +0100

    Add bias logic to forward propagation

--- neural_network.py
+++ neural_network.py
@@ -11,9 +11,14 @@ def generate_parameters(variables_number: int, hidden_neurons: int, output_neuro
     parameters['bias'] = [np.ones([hidden_neurons, 1]), np.ones([output_neurons, 1])]
     return parameters
 
-def forward_propagation(input_layer: np.array, parameters: dict):
-    hidden_layer = sigmoid(np.dot(input_layer, parameters['weights'][0]) )
-    output_layer = sigmoid(np.dot(input_layer, parameters['weights'][1]) )
+def forward_propagation(input_layer: np.array, parameters: dict, hidden_neurons: int, output_neurons: int):
+    ho = np.dot(input_layer, parameters['weights'][0])
+    for i in range(hidden_neurons): ho[i] += parameters['bias'][0][i]
+    hidden_layer = sigmoid(ho)
+
+    oo = np.dot(hidden_layer, parameters['weights'][1])
+    for i in range(output_neurons): oo[i] + parameters['bias'][1][i]
+    output_layer = sigmoid(oo)
     return [input_layer, hidden_layer, output_layer]
 
 def backward_propagation(layers: list, cost: float, parameters: dict):
@@ -29,17 +34,17 @@ def backward_propagation(layers: list, cost: float, parameters: dict):
 def train_neural_network(X: np.array, Y: np.array, epochs: int, hidden_neurons: int, output_neurons: int):
     parameters = generate_parameters(np.shape(X)[1], hidden_neurons, output_neurons)
     for epoch in range(epochs):
-        layers = forward_propagation(X, parameters)
+        layers = forward_propagation(X, parameters, hidden_neurons, output_neurons)
         output_layer = layers[2]
         cost = cost_function(np.squeeze(output_layer), Y)
         parameters = backward_propagation(layers, cost, parameters)
-        print(parameters)
     return parameters
 
 X = np.array(([0, 0], [0, 1], [1, 0], [1, 1]), dtype=float)
 y = np.array(([0], [1], [1], [0]), dtype=float)
 
 a = train_neural_network(X, y, 10, 2, 2)
+print(a)
 '''
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Neural network implementation.")
