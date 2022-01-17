03a2d017bd54e4de5873cdb50b0fff5c7db2fc35
ann-sos <anna.w.sosulska@gmail.com>
2022-01-17 11:20:39 +0100

    Refactor: data normalization, added seed, refactor forward and backpropagation.

--- neural_network.py
+++ neural_network.py
@@ -3,57 +3,80 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
-def sigmoid(z):
-    z = np.clip(z, -500, 500)  # avoid overflow
-    return 1 / (1 + np.exp(-z))
-def derivative_sigmoid(x): return x * (1 - x)
-def cost_function(Y_predicted, Y_expected): return 1 / np.shape(Y_expected)[0] * np.sum((Y_predicted - Y_expected) ** 2)
+def sigmoid(x):
+    x = np.clip(x, -500, 500)  # avoid overflow
+    return 1 / (1 + np.exp(-x))
+def derivative_sigmoid(x): 
+    x = sigmoid(x)
+    return x * (1 - x)
 
 def generate_parameters(variables_number: int, hidden_neurons: int, output_neurons: int) -> dict:
     parameters = {}
-    hidden_weights = np.random.uniform(low=-0.2, high=0.2, size=(variables_number, hidden_neurons))
-    output_weights = np.random.uniform(low=-2, high=2, size=(hidden_neurons, output_neurons))
+    np.random.seed(42)
+    hidden_weights = np.random.uniform(low=-0.2, high=0.2, size=(hidden_neurons, variables_number,))
+    output_weights = np.random.uniform(low=-2, high=2, size=(output_neurons, hidden_neurons))
     parameters['weights'] = [hidden_weights, output_weights]
-    parameters['biases'] = [np.ones([1, hidden_neurons]), np.ones([1, output_neurons])]
+    parameters['biases'] = [np.random.randn(1, hidden_neurons).squeeze(), np.random.randn(1, output_neurons).squeeze()]
     return parameters
 
 def forward_propagation(input_layer: np.array, parameters: dict):
-    hl = np.dot(input_layer, parameters['weights'][0]) + parameters['biases'][0]
+    hl = np.dot(parameters['weights'][0], input_layer) + parameters['biases'][0]
     hidden_layer = sigmoid(hl)
-    ol = np.dot(hidden_layer, parameters['weights'][1]) + parameters['biases'][1]
+    ol = np.dot(parameters['weights'][1], hidden_layer) + parameters['biases'][1]
     output_layer = sigmoid(ol)
-    return [input_layer, hidden_layer, output_layer]
+    return [input_layer, hidden_layer, output_layer, hl, ol]
 
-def backward_propagation(layers: list, cost: float, parameters: dict):
-    learning_rate = 0.1
+def backward_propagation(layers: list, Y, parameters: dict):
+    learning_rate = 0.01
+    change_w = {}
 
-    for layer_idx in range(2, 0, -1):
+    # Calculate W2 update
+    error = 2 * (layers[2] - Y) / layers[2].shape[0] * derivative_sigmoid(layers[4])
+    change_w['W2'] = np.outer(error, layers[1])
+    change_w['B2'] = error
+
+
+    # Calculate W1 update
+    error = np.dot(parameters['weights'][1].T, error) * derivative_sigmoid(layers[3])
+    change_w['W1'] = np.outer(error, layers[0])
+    change_w['B1'] = error
+    #print("change_w['B2']", change_w['B2'])
+    #Update parameters
+    parameters['weights'][0] -= learning_rate * change_w['W1']
+    parameters['biases'][0] -= learning_rate * change_w['B1']
+    parameters['weights'][1] -= learning_rate * change_w['W2']
+    parameters['biases'][1] -= learning_rate * change_w['B2']
+    
+    """for layer_idx in range(2, 0, -1):
         error_delta = cost * derivative_sigmoid(layers[layer_idx])
+        #print(f"Shape of error delta: {np.shape(error_delta)}\n")
+        #print(f"Shape of bias: {np.shape(parameters['biases'][layer_idx - 1])}\n")
         parameters['weights'][layer_idx - 1] += learning_rate * np.dot(np.transpose(layers[layer_idx - 1]), error_delta)
         parameters['biases'][layer_idx - 1] += learning_rate * np.sum(error_delta, axis=0)
-        cost = np.dot(error_delta, np.transpose(parameters['weights'][layer_idx - 1]))
+        cost = np.dot(error_delta, np.transpose(parameters['weights'][layer_idx - 1]))"""
     return parameters
 
 def train_neural_network(X: np.array, Y: np.array, epochs: int, hidden_neurons: int, output_neurons: int):
     parameters = generate_parameters(np.shape(X)[1], hidden_neurons, output_neurons)
     for epoch in range(epochs):
-        layers = forward_propagation(X, parameters)
-        #print(f"Epoch: {epoch}\nHidden layer:\n{layers[1]}\nOutput layer:\n{layers[2]}\n")
-        output_layer = layers[2]
-        cost = cost_function(np.squeeze(output_layer), Y)
-        parameters = backward_propagation(layers, cost, parameters)
-        #print(parameters)
+        for x, y in zip(X, Y):
+            layers = forward_propagation(x, parameters)
+            parameters = backward_propagation(layers, y, parameters)
+    #print("parameters", parameters)
+    #print(f"Epoch: {epoch}\nOutput layer:\n{layers[2]}\n")
     return parameters
 
 
-def calculate_accuracy(x, y, parameters):
-    total = y.shape[0]
+def calculate_accuracy(X, Y, parameters):
+    total = Y.shape[0]
     count = 0
-    for x, y in zip(x, y):
+    for x, y in zip(X, Y):
         output = forward_propagation(x, parameters)[-1]
+        #print(f"prediction: {output} \n expected: {y}\n")
+
         if output.argmax() == y.argmax():
                 count += 1
-        return count/total
+    return count/total
 
 def evaluate():
     # Import data
@@ -62,10 +85,10 @@ def evaluate():
     my_data = my_data[2:,:] # drop headers
     x = my_data[:,:-1]
     y = my_data[:,-1]
-    #x = x / np.linalg.norm(x)
+    x = x / np.linalg.norm(x)
     y = pd.get_dummies(y).to_numpy()
     x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.1)
-    parameters = train_neural_network(x_train, y_train, 100, 21, 7)
+    parameters = train_neural_network(x_train, y_train, 500, 21, 7)
     # validate
     accuracy = calculate_accuracy(x_val, y_val, parameters)
     print(f"Accuracy: {accuracy * 100}")
