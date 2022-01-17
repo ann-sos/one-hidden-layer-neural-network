46bda00b5949a0161ef144021fccb6d4ef923466
ann-sos <anna.w.sosulska@gmail.com>
2022-01-17 12:00:32 +0100

    Create plots

--- neural_network.py
+++ neural_network.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 
 def sigmoid(x):
@@ -26,8 +27,8 @@ def forward_propagation(input_layer: np.array, parameters: dict):
     output_layer = sigmoid(ol)
     return [input_layer, hidden_layer, output_layer, hl, ol]
 
-def backward_propagation(layers: list, Y, parameters: dict):
-    learning_rate = 0.01
+def backward_propagation(layers: list, Y, parameters: dict, learning_rate=0.1):
+    #learning_rate = 0.01
     change_w = {}
 
     # Calculate W2 update
@@ -40,31 +41,27 @@ def backward_propagation(layers: list, Y, parameters: dict):
     error = np.dot(parameters['weights'][1].T, error) * derivative_sigmoid(layers[3])
     change_w['W1'] = np.outer(error, layers[0])
     change_w['B1'] = error
-    #print("change_w['B2']", change_w['B2'])
     #Update parameters
     parameters['weights'][0] -= learning_rate * change_w['W1']
     parameters['biases'][0] -= learning_rate * change_w['B1']
     parameters['weights'][1] -= learning_rate * change_w['W2']
     parameters['biases'][1] -= learning_rate * change_w['B2']
     
-    """for layer_idx in range(2, 0, -1):
-        error_delta = cost * derivative_sigmoid(layers[layer_idx])
-        #print(f"Shape of error delta: {np.shape(error_delta)}\n")
-        #print(f"Shape of bias: {np.shape(parameters['biases'][layer_idx - 1])}\n")
-        parameters['weights'][layer_idx - 1] += learning_rate * np.dot(np.transpose(layers[layer_idx - 1]), error_delta)
-        parameters['biases'][layer_idx - 1] += learning_rate * np.sum(error_delta, axis=0)
-        cost = np.dot(error_delta, np.transpose(parameters['weights'][layer_idx - 1]))"""
     return parameters
 
-def train_neural_network(X: np.array, Y: np.array, epochs: int, hidden_neurons: int, output_neurons: int):
+def train_neural_network(X: np.array, Y: np.array, X_test: np.array, Y_test: np.array, epochs: int, hidden_neurons: int, output_neurons: int, learning_rate=0.1, test_on_the_go=False):
+    accuracy_list = []
     parameters = generate_parameters(np.shape(X)[1], hidden_neurons, output_neurons)
     for epoch in range(epochs):
         for x, y in zip(X, Y):
             layers = forward_propagation(x, parameters)
-            parameters = backward_propagation(layers, y, parameters)
-    #print("parameters", parameters)
-    #print(f"Epoch: {epoch}\nOutput layer:\n{layers[2]}\n")
-    return parameters
+            parameters = backward_propagation(layers, y, parameters, learning_rate)
+        if epoch % 10 == 0 and test_on_the_go:
+            accuracy = calculate_accuracy(X_test, Y_test, parameters)
+            accuracy_list.append(accuracy)
+            print(f"Epoch: {epoch} --- Accuracy: {accuracy*100:.2f}%")
+
+    return parameters, accuracy_list
 
 
 def calculate_accuracy(X, Y, parameters):
@@ -72,15 +69,12 @@ def calculate_accuracy(X, Y, parameters):
     count = 0
     for x, y in zip(X, Y):
         output = forward_propagation(x, parameters)[-1]
-        #print(f"prediction: {output} \n expected: {y}\n")
-
         if output.argmax() == y.argmax():
                 count += 1
     return count/total
 
-def evaluate():
+def import_data(file):
     # Import data
-    file = r'winequality-white.csv'
     my_data = np.genfromtxt(file, delimiter=';')
     my_data = my_data[2:,:] # drop headers
     x = my_data[:,:-1]
@@ -88,18 +82,45 @@ def evaluate():
     x = x / np.linalg.norm(x)
     y = pd.get_dummies(y).to_numpy()
     x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.1)
-    parameters = train_neural_network(x_train, y_train, 500, 21, 7)
+    return x_train, x_val, y_train, y_val
+
+
+def evaluate(file, learning_rate=0.01):
+    x_train, x_val, y_train, y_val = import_data(file)
+    parameters, accuracy_lst = train_neural_network(x_train, y_train, x_val, y_val, 100, 21, 7)
     # validate
     accuracy = calculate_accuracy(x_val, y_val, parameters)
-    print(f"Accuracy: {accuracy * 100}")
+    print(f"Accuracy: {accuracy * 100:.2f}")
+    return accuracy_lst
 
-print(evaluate())
-'''
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Neural network implementation.")
-    parser.add_argument("-f", "--file", required=True, help="path to the file containing dataset")
-    parser.add_argument("-i", "--input", type=int, help="number of input neurons", required=True)
-    parser.add_argument("-hd", "--hidden", type=int, help="number of hidden neurons", required=True)
-    parser.add_argument("-o", "--output", type=int, help="number of output neurons", required=True)
-    args = vars(parser.parse_args())
-'''
+#Test learning rate influence on accuracy
+x_train, x_val, y_train, y_val = import_data(r'winequality-white.csv')
+
+"""plt.figure(1)
+for learning_rate in [0.001, 0.01, 0.5]:
+    parameters, accuracy_lst = train_neural_network(x_train, y_train, x_val, y_val, 200, 21, 7, learning_rate, test_on_the_go=True)
+    plt.plot(np.arange(0, len(accuracy_lst)*10, 10), accuracy_lst, label=f"learning_rate={learning_rate}")
+plt.title("Neural network")
+plt.xlabel("Epoch [-]")
+plt.ylabel("Accuracy [%]")
+plt.legend()
+plt.savefig("Neural_network_accuracy_epoch_learning_rate")
+
+plt.figure(2)
+for hidden_neurons in [14, 28, 56]:
+    parameters, accuracy_lst = train_neural_network(x_train, y_train, x_val, y_val, 200, hidden_neurons, 7, test_on_the_go=True)
+    plt.plot(np.arange(0, len(accuracy_lst)*10, 10), accuracy_lst, label=f"hidden_neurons={hidden_neurons}")
+plt.title("Neural network")
+plt.xlabel("Epoch [-]")
+plt.ylabel("Accuracy [%]")
+plt.legend()
+plt.savefig("Neural_network_accuracy_epoch_hidden")"""
+
+
+plt.figure(3)
+parameters, accuracy_lst = train_neural_network(x_train, y_train, x_val, y_val, 1000, 21, 7, test_on_the_go=True)
+plt.plot(np.arange(0, len(accuracy_lst)*10, 10), accuracy_lst)
+plt.title("Neural network")
+plt.xlabel("Epoch [-]")
+plt.ylabel("Accuracy [%]")
+plt.savefig("Neural_network_accuracy_epoch_long")
